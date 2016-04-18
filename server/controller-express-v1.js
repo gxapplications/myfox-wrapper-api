f13840389289ec4e722fe26a1996da3c0bcb5089
gxapplications <xavier.gouley@gmail.com>
2016-04-19 00:17:25 +0200

    wrapper factory

--- server/controller-express-v1.js
+++ server/controller-express-v1.js
@@ -1,10 +1,13 @@
 'use strict';
 
 import { Router } from 'express';
+import api, { rest } from '../lib/index';
 
 const router = new Router();
 
 router.get('/test/:vari', function test(request, reply, next) {
+    console.log(api().callScenario('123', () => {}));
+    console.log(rest().callScenario('123', () => {}));
     reply.send("test: " + request.params.vari);
 });
 
