df6877e5b0c4164334583d0d2d05d32136a8873d
gxapplications <xavier.gouley@gmail.com>
2016-04-21 01:15:20 +0200

    test coverage and utf8

--- test/lib/wrapper-api-test.js
+++ test/lib/wrapper-api-test.js
@@ -1,9 +1,40 @@
+'use strict';
+
 import { expect } from 'chai';
 
-import MyfoxWrapperApi from '../../lib/index';
+import MyfoxWrapperApi, { rest as restApi, html as htmlApi} from '../../lib/index';
 
-describe('wrapper-api library', () => {
-    it('stupid test', () => {
-        expect(1).to.equal(1);
+describe('wrapper-api library instantiation', () => {
+    it('Default instance should have \'htmlFirst\' strategy', () => {
+        const api = MyfoxWrapperApi();
+        expect(api.options.apiStrategy).to.equal('htmlFirst');
     });
+
+    it('Direct call to rest-api instance must have \'custom\' strategy', () => {
+        const api = restApi();
+        expect(api.options.apiStrategy).to.equal('custom');
+    });
+    it('Direct call to html-api instance must have \'custom\' strategy', () => {
+        const api = restApi();
+        expect(api.options.apiStrategy).to.equal('custom');
+    });
+
+    it('Indirect call to rest-api instance must have \'custom\' strategy', () => {
+        const api = MyfoxWrapperApi.rest();
+        expect(api.options.apiStrategy).to.equal('custom');
+    });
+    it('Indirect call to html-api instance must have \'custom\' strategy', () => {
+        const api = MyfoxWrapperApi.html();
+        expect(api.options.apiStrategy).to.equal('custom');
+    });
+
+    it('Instance with restFirst strategy must contains a Html fallback strategy');
+    it('Instance with restOnly strategy must not contains a fallback strategy');
+    it('Instance with htmlFirst strategy must contains a Rest fallback strategy');
+    it('Instance with htmlOnly strategy must not contains a fallback strategy');
+});
+
+describe('wrapper-api library options during instantiation', () => {
+    it('Default options must be accepted');
+    it('Wrong options must be rejected');
 });
---
+++ test/server/express-test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import { expect } from 'chai';
+
+describe('express application', () => {
+    it('todo');
+});
+
+describe('express controller', () => {
+    it('todo');
+});
