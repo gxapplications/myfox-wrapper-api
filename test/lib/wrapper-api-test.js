3951ca04d405e93a6b60ffc634ec0d71493ec01e
gxapplications <xavier.gouley@gmail.com>
2016-04-21 00:15:42 +0200

    test and travis

---
+++ test/lib/wrapper-api-test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+import MyfoxWrapperApi from '../../lib/index';
+
+describe('wrapper-api library', () => {
+    it('stupid test', () => {
+        expect(1).to.equal(1);
+    });
+});
