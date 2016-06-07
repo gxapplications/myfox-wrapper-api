5e013e096a1883738fe3aef0a55e9519fbd4537b
Xavier Gouley <xavier.gouley@prestashop.com>
2016-06-07 17:02:50 +0200

    // home parser tests

--- test/lib/html-parsers/home-test.js
+++ test/lib/html-parsers/home-test.js
@@ -1,10 +1,27 @@
 /* eslint-env mocha */
 'use strict'
 
+import fs from 'fs'
+import path from 'path'
+
+import CommonApi from '../../../lib/common-api'
+import homeParser from '../../../lib/html-parsers/home'
+
 describe('HTML Home parser', () => {
-  // TODO !0: TU: maybe 3-4 tests with different cases...
-  it('can parse a whole page and delivers data')
-  // TODO !1: 2 mocks with specific differences. Test differences: if triggered in common-api.
-  it('can parse a whole page, then a new one, and triggers persistentState changes')
+  it('can parse a whole page and delivers data', (done) => {
+    let api = new CommonApi({'myfoxSiteIds': [1234]})
+    let tr = homeParser(api)
+    tr.on('end', () => {
+      done()
+    })
+    tr.on('error', done)
+    // TODO !0: TU: maybe 3-4 tests with different cases...
+    fs.createReadStream(path.join(__dirname, 'mock-home.html')).pipe(tr)
+  })
+
+  it('can parse a whole page, then a new one, and triggers persistentState changes', (done) => {
+    // TODO !1: 2 mocks with specific differences. Test differences: if triggered in common-api.
+    done()
+  })
 })
 
