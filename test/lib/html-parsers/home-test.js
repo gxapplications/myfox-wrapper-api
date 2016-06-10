270bfb4df6c7cbd77dee595f40dda4573d9f9cde
Xavier Gouley <xavier.gouley@prestashop.com>
2016-06-10 08:56:33 +0200

    // home parser tests OK

--- test/lib/html-parsers/home-test.js
+++ test/lib/html-parsers/home-test.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 'use strict'
 
+import { expect } from 'chai'
 import fs from 'fs'
 import path from 'path'
 
@@ -15,13 +16,80 @@ describe('HTML Home parser', () => {
       done()
     })
     tr.on('error', done)
-    // TODO !0: TU: maybe 3-4 tests with different cases...
     fs.createReadStream(path.join(__dirname, 'mock-home.html')).pipe(tr)
   })
 
   it('can parse a whole page, then a new one, and triggers persistentState changes', (done) => {
-    // TODO !1: 2 mocks with specific differences. Test differences: if triggered in common-api.
-    done()
+    let api = new CommonApi({'myfoxSiteIds': [1234]})
+    let listenerNotifications = []
+    let listener = (label, value, oldValue, timestamp) => {
+      switch (label) {
+        case 'status':
+          expect(value).to.deep.equal({ internet: '1', powerSupply: '1', diagnosis: '1' })
+          expect(oldValue).to.deep.equal({ internet: '0', powerSupply: '0', diagnosis: '0' })
+          break
+        case 'alarm':
+          expect(value).to.equal('half')
+          expect(oldValue).to.equal('full')
+          break
+        case 'scenarii':
+          expect(value).to.deep.equal({
+            '654': { type: '>1', name: 'Sc activable', active: '1' },
+            '987': { type: 1, name: 'Sc playable' }
+          })
+          expect(oldValue).to.deep.equal({
+            '654': { type: '>1', name: 'Sc activable', active: '0' },
+            '987': { type: 1, name: 'Sc playable' }
+          })
+          break
+        case 'data':
+          expect(value).to.deep.equal({
+            '258': { name: 'Capteur 1', temperature: '20.8°', light: 'Pénombre' },
+            '259': { name: 'Capteur 2', temperature: '20.2°', light: 'Pénombre' }
+          })
+          expect(oldValue).to.deep.equal({
+            '258': { name: 'Capteur 1', temperature: '22.1°', light: 'Lumière du jour' },
+            '259': { name: 'Capteur 2', temperature: '20.2°', light: 'Pénombre' }
+          })
+          break
+        case 'heat':
+          expect(value).to.deep.equal({
+            '965': { name: 'Ch 2', state: 'eco' },
+            '2171': { name: 'Ch 1', state: 'off' }
+          })
+          expect(oldValue).to.deep.equal({
+            '965': { name: 'Ch 2', state: 'eco' },
+            '2171': { name: 'Ch 1', state: 'eco' }
+          })
+      }
+      listenerNotifications.push(label)
+    }
+    api.addStateListener('status', listener)
+    api.addStateListener('alarm', listener)
+    api.addStateListener('scenarii', listener)
+    api.addStateListener('data', listener)
+    api.addStateListener('heat', listener)
+
+    let tr1 = homeParser(api)
+
+    tr1.on('end', () => {
+      let tr2 = homeParser(api)
+      tr2.on('end', () => {
+        expect(listenerNotifications).to.have.lengthOf(5)
+
+        let tr3 = homeParser(api)
+        tr3.on('end', () => {
+          expect(listenerNotifications).to.have.lengthOf(5)
+          done()
+        })
+        tr3.on('error', done)
+        fs.createReadStream(path.join(__dirname, 'mock-home-2.html')).pipe(tr3)
+      })
+      tr2.on('error', done)
+      fs.createReadStream(path.join(__dirname, 'mock-home-2.html')).pipe(tr2)
+    })
+    tr1.on('error', done)
+    fs.createReadStream(path.join(__dirname, 'mock-home-1.html')).pipe(tr1)
   })
 })
 
