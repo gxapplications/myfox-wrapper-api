091ac6b88fadb49bf1fa8f796937e921b24530c8
Xavier Gouley <xavier.gouley@prestashop.com>
2016-06-17 17:18:36 +0200

    // TU

--- test/lib/macro-listener-test.js
+++ test/lib/macro-listener-test.js
@@ -5,10 +5,16 @@
 
 // import CommonApi from '../../lib/common-api'
 
-// TODO !0 : doc and TU on addMacroListener + notifyMacroListeners
+// TODO !0 : doc on addMacroListener + notifyMacroListeners
 describe('Macro listener behavior', () => {
-  it('is correct for adding-deleting listener')
-  it('is correct for readding an already registered listener')
+  it('is correct for adding-deleting listener', () => {
+    // let api = new CommonApi({'myfoxSiteIds': [1234]})
+    // let listener = (id, data, state, remaining, timestamp) => { }
+    // expect(api.addMacroListener(listener)).to.be.true
+    // expect(api.removeMacroListener(listener)).to.be.true
+  })
+
+  it('is correct for re-adding an already registered listener')
   describe('triggers listeners when an event occurs', () => {
     it('contains the right data')
     it('is kept after the event')
